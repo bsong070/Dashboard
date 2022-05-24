98488e014ab583aa8fe1c66872377d429ac448db
Brian <bsong070@gmail.com>
2022-05-23 23:04:08 -0400

    heroku push

--- notesfront/settings.py
+++ notesfront/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+# import django_heroku
+import django_on_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,7 +126,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
+django_on_heroku.settings(locals())
+
 
 STATICFILES_DIR = [BASE_DIR/"static"]
 
